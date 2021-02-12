0c7ccc2c1b354f10696aa1ac5a3ccb65a0ae6906
Ieuan Skinner <ieuanskinner@gmail.com>
2021-02-12 18:05:35 +0000

    DT-47 Removed redundant README file and refactored header/footer components to reduce the amount of unused style rules that exist.

--- lilw-react-components/src/core.stories.js
+++ lilw-react-components/src/core.stories.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from 'react'
+import { storiesOf } from '@storybook/react'
 import './suds.scss'
+import facebook from './images/facebook.png'
+import google from './images/google.png'
+import twitter from './images/twitter.png'
 
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/header'
+import Footer from './components/footer'
+import {Col, Row} from 'react-bootstrap'
 
 storiesOf('Core', module)
   .add('Header', () => <Header a11y_heading_desc={'Component Example'}/>)
   .add('Footer', () => <Footer/>)
-  .add('Footer + Additional Logo', () =>
-      <Footer additional_logo={ <img alt='Read Contract Logo' src='https://www.readcontract.com/img/readcontract-logo.png'/> }/>);
+  .add('Footer + Extra Logo', () =>
+      <Footer logos={
+        <Row>
+          <Col>
+            <img alt='Google' src={google}/>
+          </Col>
+        </Row>
+      }/>)
+  .add('Footer + Multiple Logos', () =>
+      <Footer logos={
+        <Row>
+          <Col>
+            <img alt='Facebook' src={facebook}/>
+          </Col>
+          <Col>
+            <img alt='Twitter' src={twitter}/>
+          </Col>
+          <Col>
+            <img alt='Google' src={google}/>
+          </Col>
+        </Row>
+      }/>)
